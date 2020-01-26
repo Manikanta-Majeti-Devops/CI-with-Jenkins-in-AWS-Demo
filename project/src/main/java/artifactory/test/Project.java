91916cb0b1098828dabbd1c8f205c49a2dcaf4a6
Manikanta-Majeti-Devops <58879583+Manikanta-Majeti-Devops@users.noreply.github.com>
2020-01-26 17:21:33 +0000

    GHE_SQUBE_Jenkins_Integration

--- project/src/main/java/artifactory/test/Project.java
+++ project/src/main/java/artifactory/test/Project.java
@@ -1,7 +1,7 @@
 package artifactory.test;
 
 /**
- * Hello world! Have a good day!
+ * Hello world! Have a good day! It is a rainy day here!
  */
 public class Project {
     public static void main(String[] args) {
